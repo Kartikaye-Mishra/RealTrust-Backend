b1753635ff303142061c45be4aa00396cfb858f5
Kartikaye-Mishra <kartikaye1.mishra@gmail.com>
2024-11-09 22:42:11 +0530

    added url

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration to allow only the frontend URL
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://realtrust-backend.onrender.com',
   optionsSuccessStatus: 200 // For legacy browser support
 };
 app.use(cors(corsOptions));
